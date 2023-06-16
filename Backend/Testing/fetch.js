b7dba05ca7b0bf661fc6e2cf827565e7bb17fe71
AidenLYT <ytstirlingite@hotmail.com>
2023-06-17 02:56:02 +0800

    new query functions

--- Backend/Testing/fetch.js
+++ Backend/Testing/fetch.js
@@ -2,75 +2,60 @@ import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import admin from "firebase-admin";
+import serviceAccount from "/Users/matchaboii/CardioHardio/serviceAccountKey.json" assert { type: 'json'};
+import { getFirestore } from 'firebase-admin/firestore';
 
 // Queried field
-export async function fetchAll() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyDcVtckd1tAUHiWcIvXIorBN6uOXTWKoDA",
-        authDomain: "cardiohardio-6dbc7.firebaseapp.com",
-        databaseURL: "https://cardiohardio-6dbc7-default-rtdb.firebaseio.com",
-        projectId: "cardiohardio-6dbc7",
-        storageBucket: "cardiohardio-6dbc7.appspot.com",
-        messagingSenderId: "363762702221",
-        appId: "1:363762702221:web:267ce56ee96124d27b8fc4",
-        measurementId: "G-K982FE9FLR"
-    };
-    
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    };
-    
-    const db = firebase.firestore();
-    
-    const fetchAllFun = () => {
-        useEffect(async () => {
-            const docRef = db.collection('UserData');
-            const docSnapshot = await docRef.get();
-            
-            // Array of all datas
-            const data = [];
-            docSnapshot.forEach(doc => {
-                const docData = doc.data();
-                const entry = {
-                    date: doc.id,
-                    bmi: docData.bmi,
-                };
-                data.push(entry);
+export async function fetchAll(queried) {
+    try {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://cardiohardio-6dbc7-default-rtdb.firebaseio.com"
+        });
+
+        const db = getFirestore();
+        const docRef = db.collection('UserData');
+        const docSnapshot = await docRef.get();
+        
+        if (docSnapshot.exists) {
+            const allField = [];
+            docSnapshot.forEach((doc) => {
+                const field = doc.data();
+                if (field.hasOwnProperty(queried)) {
+                    allField.push(field[queried])
+                }
             });
-            return data;
-        }, []);
+            return allField;
+        } else {
+            console.log('Doc not found');
+        }
+        
+    } catch (error) {
+        console.error('Cannot obtain data', error);
     }
-    return fetchAllFun;
 }
 
 // Queried field and date
 export async function fetchOne(queried, date) {
-    const firebaseConfig = {
-        apiKey: "AIzaSyDcVtckd1tAUHiWcIvXIorBN6uOXTWKoDA",
-        authDomain: "cardiohardio-6dbc7.firebaseapp.com",
-        databaseURL: "https://cardiohardio-6dbc7-default-rtdb.firebaseio.com",
-        projectId: "cardiohardio-6dbc7",
-        storageBucket: "cardiohardio-6dbc7.appspot.com",
-        messagingSenderId: "363762702221",
-        appId: "1:363762702221:web:267ce56ee96124d27b8fc4",
-        measurementId: "G-K982FE9FLR"
-    };
-    
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-    };
-    
-    const db = firebase.firestore();
+    try {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: "https://cardiohardio-6dbc7-default-rtdb.firebaseio.com"
+        });
 
-    const fetchOneFun = () => {
-        useEffect(async () => {
-            const docRef = db.collection('UserData').doc(date);
-            const docSnapshot = await docRef.get();
-            if (docSnapshot.exists) { 
-                const enquiry = queried;
-                return docSnapshot.get(enquiry);
-            }
-        }, []);
+        const db = getFirestore();
+        const docRef = db.collection('UserData').doc(date);
+        const docSnapshot = await docRef.get();
+        
+        if (docSnapshot.exists) {
+            const field = docSnapshot.data();
+            const fieldValue = field[queried];
+            return fieldValue;
+        } else {
+            console.log('Doc not found');
+        }
+    } catch (error) {
+        console.error('Cannot obtain data', error);
     }
-    return fetchOneFun;
 }
